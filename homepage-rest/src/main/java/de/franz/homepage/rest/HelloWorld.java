fe1c6a3365e811e630a3426ca37477518aad158f
jens <j@f.de>
2014-05-30 18:18:07 +0200

    Example Rest API with container managed security

---
+++ homepage-rest/src/main/java/de/franz/homepage/rest/HelloWorld.java
@@ -0,0 +1,15 @@
+package de.franz.homepage.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("/helloworld")
+public class HelloWorld {
+    @GET
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        return "Hello World";
+    }
+}
