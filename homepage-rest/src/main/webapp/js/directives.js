72bdb1643df5a7ec75ba1a7ede935a3024d771a9
jens <dark.nexus6@gmail.com>
2014-06-08 11:07:33 +0200

    Menu active class according to location path

--- homepage-rest/src/main/webapp/js/directives.js
+++ homepage-rest/src/main/webapp/js/directives.js
@@ -25,9 +25,28 @@ angular.module('myApp.directives', [])
         return {
             templateUrl: '../templates/menu.html',
             restrict: 'E',
-            replace: true,
-            link: function (scope, element, attrs) {
-
+            replace: true
+        };
+    })
+/**
+ * Directive for dynamically decide if the current menu item is active. This will be accomplished by comparing the
+ * href attribute with the current location. Then the class active will be added on the element.
+ */
+    .directive('activeLink', ['$location', function (location) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs, controller) {
+                var clazz = attrs.activeLink || 'active';
+                var path = element.find("a").attr('href');
+                path = path.substring(1); //hack because path does not return including hashbang
+                scope.location = location;
+                scope.$watch('location.path()', function (newPath) {
+                    if (path === newPath) {
+                        element.addClass(clazz);
+                    } else {
+                        element.removeClass(clazz);
+                    }
+                });
             }
         };
-    });
+    }]);
