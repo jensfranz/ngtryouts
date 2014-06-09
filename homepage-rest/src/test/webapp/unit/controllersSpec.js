28f04074022a94e06f9d7e7f13cdcbc703e95cea
jens <dark.nexus6@gmail.com>
2014-06-09 17:48:52 +0200

    Corrected Controller tests.

--- homepage-rest/src/test/webapp/unit/controllersSpec.js
+++ homepage-rest/src/test/webapp/unit/controllersSpec.js
@@ -6,15 +6,15 @@ describe('controllers', function () {
     beforeEach(module('myApp.controllers'));
 
 
-    it('should ....', inject(function ($controller) {
+    it('should have a HomeCtrl', inject(function ($controller) {
         //spec body
-        var myCtrl1 = $controller('MyCtrl1', { $scope: {} });
-        expect(myCtrl1).toBeDefined();
+        var homeCtrl = $controller('HomeCtrl', { $scope: {} });
+        expect(homeCtrl).toBeDefined();
     }));
 
-    it('should ....', inject(function ($controller) {
+    it('should have a AboutCtrl', inject(function ($controller) {
         //spec body
-        var myCtrl2 = $controller('MyCtrl2', { $scope: {} });
-        expect(myCtrl2).toBeDefined();
+        var aboutCtrl = $controller('AboutCtrl', { $scope: {} });
+        expect(aboutCtrl).toBeDefined();
     }));
 });
