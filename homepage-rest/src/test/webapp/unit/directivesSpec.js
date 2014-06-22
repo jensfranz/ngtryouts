47a078bc256ff19a5ccae650d5f2cb72f6dc4188
Jens Franz <dark.nexus6@gmail.com>
2014-06-22 19:07:40 +0200

    loging-directive spec.

--- homepage-rest/src/test/webapp/unit/directivesSpec.js
+++ homepage-rest/src/test/webapp/unit/directivesSpec.js
@@ -16,4 +16,28 @@ describe('directives', function () {
             });
         });
     });
+
+    describe('login', function () {
+        var $compile, $rootScope;
+
+        beforeEach(module('preprocessor-templates'));
+
+        beforeEach(inject(function (_$compile_, _$rootScope_) {
+            // The injector unwraps the underscores (_) from around the parameter names when matching
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        }));
+
+        it('should replace the login-element by the template', function () {
+            // Compile a piece of HTML containing the directive
+            var element = $compile('<login></login>')($rootScope);
+            // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+            $rootScope.$digest();
+            // Check that the compiled element contains the templated content
+            expect(element.find('input[ng-model="credentials.user"]').length).toBe(1);
+            expect(element.find('input[ng-model="credentials.password"]').length).toBe(1);
+            // be sure, that there is no false configuration
+            expect(element.find('input[ng-model="credentials.user2"]').length).toBe(0);
+        });
+    });
 });
